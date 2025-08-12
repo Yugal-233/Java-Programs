f43ae1867808bbdf74a9d967adf520bfb270e06b
Mayur <mayurchaudhari8080@gmail.com>
2025-08-12 22:34:51 +0530

    Added this file-mayurifirstprogram

---
+++ src/com/java/programs/mayurifirstprogram.java
@@ -0,0 +1,7 @@
+package com.java.programs;
+
+public class mayurifirstprogram {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mayuri! This is your first Java program.");
+    }
+}
