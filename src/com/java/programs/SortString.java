63ac117f63d1fffd78e532ba5541c2aeb35163a7
Mayur <mayurchaudhari8080@gmail.com>
2025-12-15 23:11:11 +0530

    updated SortString

--- src/com/java/programs/SortString.java
+++ src/com/java/programs/SortString.java
@@ -10,17 +10,17 @@ public class SortString {
 
         char[] ch1 = str.toCharArray();
         Arrays.sort(ch1);
-        for (int i = 0; i < ch1.length; i++) {
-            System.out.print(ch1[i]+" ");
+        for (char value : ch1) {
+            System.out.print(value + " ");
         }
         System.out.println();
         System.out.println("******************************");
 
-        char[] ch = str.toCharArray();
-        char temp = ' ';
-        for (int i = 0; i < ch.length; i++) {
-            for (int j = 0; j < ch.length; j++) {
-                if(ch[i]<ch[j]){
+        char [] ch = str.toCharArray();
+        char temp;
+        for(int i=0;i<ch.length;i++) {
+            for(int j=i+1;j<ch.length;j++) {
+                if(ch[i]>ch[j]) {
                     temp=ch[i];
                     ch[i]=ch[j];
                     ch[j]=temp;
