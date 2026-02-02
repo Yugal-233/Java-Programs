c08c163182d59a72b68d1a30809751f6af14a05d
Mayur <mayurchaudhari8080@gmail.com>
2026-02-02 23:36:08 +0530

    updated code changes

--- src/com/java/design/pattern/PrototypeDesignPatternDemo.java
+++ src/com/java/design/pattern/PrototypeDesignPatternDemo.java
@@ -1,47 +1,23 @@
 package com.java.design.pattern;
 
-class NetworkConnection implements Cloneable{
-    private String ip;
-    private String impData;
-    public String getIp() {
-        return ip;
-    }
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-    public String getImpData() {
-        return impData;
-    }
-    public void setImpData(String impData) {
-        this.impData = impData;
-    }
-    public void loadImportantData(){
-        this.impData="very very imp data";
-    }
-
-    @Override
-    public String toString() {
-        return "NetworkConnection{" +
-                "ip='" + ip + '\'' +
-                ", impData='" + impData + '\'' +
-                '}';
-    }
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
+class Network implements Cloneable{
+    int ipAdress;
+    String data;
+    public Object clone() throws CloneNotSupportedException {
         return super.clone();
+
     }
 }
 public class PrototypeDesignPatternDemo {
     public static void main(String[] args) throws CloneNotSupportedException {
-        NetworkConnection connection =  new NetworkConnection();
-        connection.setIp("10.119.20.112");
-        connection.loadImportantData();
-        System.out.println(connection);
-
+        Network network = new Network();
+        network.ipAdress=101021;
+        network.data="yugals data";
+        System.out.println(network.data+" "+network.ipAdress);
 
         //creating a new obj by copying existing one
-        NetworkConnection connection1 = (NetworkConnection) connection.clone();
-        System.out.println(connection1);
-
+        Network network1 = (Network) network.clone();
+        System.out.println(network1.data+" "+network1.ipAdress);
     }
+
 }
