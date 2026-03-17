dda74e94a59e2a326aa91899d25558a76b227655
mayuri <mayuripatil3061@gmail.com>
2026-03-17 23:12:51 +0530

    updated code changes

--- src/com/java/programs/InfiniteLoop.java
+++ src/com/java/programs/InfiniteLoop.java
@@ -3,7 +3,7 @@ package com.java.programs;
 public class InfiniteLoop {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i=i*3) {
-            System.out.println("Yuval");
+            System.out.println("Yugal");
         }
     }
 }
