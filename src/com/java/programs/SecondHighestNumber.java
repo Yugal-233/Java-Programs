c59c24018cde1c593052e0f9baa5b153eb03f34a
Mayur <mayurchaudhari8080@gmail.com>
2026-03-25 08:47:08 +0530

    Changes in SecondHighestNumber class

--- src/com/java/programs/SecondHighestNumber.java
+++ src/com/java/programs/SecondHighestNumber.java
@@ -5,19 +5,18 @@ import java.util.Arrays;
 public class SecondHighestNumber {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{10, 50, -63, 40, 98, -21, 56, 73, -42};
+        int[] arr = new int[]{10, 50, -63, 40, 98, -21, 73, 56, -42};
         int temp;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-
-        System.out.println(arr[1]);
+        System.out.println(arr[arr.length - 2]);
 
         System.out.println("*****************");
 
