c09a6fd2d269b7ffcce7fea1f1870316874c2285
Pro.fes.sor <yugalchaudhari233@gmail.com>
2026-04-19 19:21:31 +0530

    changes in FindPairsWithSum class

--- src/com/java/programs/FindPairsWithSum.java
+++ src/com/java/programs/FindPairsWithSum.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class FindPairsWithSum {
     public static void main(String[] args) {
         List<Integer> myList = Arrays.asList(10, 20, 30, 50, 40, 8, 60);
-        int targetSum = 30;
+        int targetSum = 60;
 
         for (int i = 0; i < myList.size(); i++) {
-            for (int j = i + 1; j < myList.size(); j++) {
+            for (int j = 0; j < myList.size(); j++) {
                 int num1 = myList.get(i);
                 int num2 = myList.get(j);
                 if (num1 + num2 == targetSum) {
